7713cafaa1694328be496e65a09101483e28aa76
Yi Zhou <yi.zhou@verystar.cn>
2019-03-07 17:20:10 +0800

    add bytes os_exec

--- main.go
+++ main.go
@@ -1,5 +1,50 @@
 package main
 
-func main() {
+import "sync"
 
+type Lock struct {
+	c chan struct{}
+}
+
+func NewLock() Lock {
+	var l Lock
+	l.c = make(chan struct{}, 1)
+	l.c <- struct{}{}
+	return l
+}
+
+func (l Lock) Lock() bool {
+	lockResult := false
+	select {
+	case <-l.c:
+		lockResult = true
+	}
+
+	return lockResult
+}
+
+func (l Lock) Unlock() {
+	l.c <- struct{}{}
+}
+
+var counter int
+
+func main() {
+	var l = NewLock()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if !l.Lock() {
+				// log error
+				println("lock failed")
+				return
+			}
+			counter++
+			println("current counter", counter)
+			l.Unlock()
+		}()
+	}
+	wg.Wait()
 }
