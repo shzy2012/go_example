fcd250526a2da597b17b1c44d9a245bdfba666e6
Joey <Joey@Xiayanhuangs-MacBook-Pro.local>
2019-05-21 15:26:10 +0800

    update cron

--- cron.go
+++ cron.go
@@ -15,14 +15,39 @@ import (
 | ---------- 小时 (0 - 23)
 ------------ 分钟 (0 - 59)
 */
-func main() {
+
+/*
+	Entry                  | Description                                | Equivalent To
+	-----                  | -----------                                | -------------
+	@yearly (or @annually) | Run once a year, midnight, Jan. 1st        | 0 0 0 1 1 *
+	@monthly               | Run once a month, midnight, first of month | 0 0 0 1 * *
+	@weekly                | Run once a week, midnight between Sat/Sun  | 0 0 0 * * 0
+	@daily (or @midnight)  | Run once a day, midnight                   | 0 0 0 * * *
+	@hourly                | Run once an hour, beginning of hour        | 0 0 * * * *
+*/
+
+/*
+Field name   | Mandatory? | Allowed values  | Allowed special characters
+----------   | ---------- | --------------  | --------------------------
+Seconds      | Yes        | 0-59            | * / , -
+Minutes      | Yes        | 0-59            | * / , -
+Hours        | Yes        | 0-23            | * / , -
+Day of month | Yes        | 1-31            | * / , - ?
+Month        | Yes        | 1-12 or JAN-DEC | * / , -
+Day of week  | Yes        | 0-6 or SUN-SAT  | * / , - ?
+*/
+func job(){
     i := 0
     c := cron.New()
-    spec := "0 0 16 * * *"
+    spec := "0 0 15 * * *" //19点 5-10分之内，每分钟发次
     c.AddFunc(spec, func() {
         i++
         log.Println("start", i)
     })
     c.Start()
+}
+func main() {
+    
+    go job()
     select{} //阻塞主线程不退出
 }
